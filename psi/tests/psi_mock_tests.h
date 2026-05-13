0dc4bcbd727465fd680c65c6532d21fdd81371c6
darkessence87 <darkessence87@gmail.com>
2026-05-13 15:16:15 +0200

    test: extend psi_mock_tests to 100% function coverage
    Add 14 new tests covering: EXPECT_CONTAINS, EXPECT_EQ<vector/map>,
    ASSERT_GE/EQ/TRUE/FALSE, EXPECT_NE<ptr/shared_ptr>, EXPECT_FALSE,
    ASSERT_FALSE, EXPECT_EQ<char8_t/wstring/bool/string>,
    detail::ptr_to_str
    
    psi_mock.h: 44.74% -> 100% functions

--- psi/tests/psi_mock_tests.h
+++ psi/tests/psi_mock_tests.h
@@ -43,4 +43,99 @@ TEST(EXPECT_CALL, with_args)
     mock->fn()(10.0);
 }
 
+TEST(psi_mock, EXPECT_CONTAINS_pass)
+{
+    EXPECT_CONTAINS(std::string("hello world"), std::string("world"));
+}
+
+TEST(psi_mock, EXPECT_EQ_vector_equal)
+{
+    std::vector<int> a {1, 2, 3};
+    std::vector<int> b {1, 2, 3};
+    EXPECT_EQ(a, b);
+}
+
+TEST(psi_mock, EXPECT_EQ_map_equal)
+{
+    std::map<int, int> a {{1, 10}, {2, 20}};
+    std::map<int, int> b {{1, 10}, {2, 20}};
+    EXPECT_EQ(a, b);
+}
+
+TEST(psi_mock, ASSERT_GE_pass)
+{
+    ASSERT_GE(5, 3);
+}
+
+TEST(psi_mock, ASSERT_EQ_wstring_pass)
+{
+    std::wstring a = L"hello";
+    std::wstring b = L"hello";
+    ASSERT_EQ(a, b);
+}
+
+TEST(psi_mock, EXPECT_NE_pointers_pass)
+{
+    int x = 1, y = 2;
+    EXPECT_NE(&x, &y);
+}
+
+TEST(psi_mock, EXPECT_NE_nullptr_shared_ptr_pass)
+{
+    auto sp = std::make_shared<int>(42);
+    EXPECT_NE(nullptr, sp);
+}
+
+TEST(psi_mock, EXPECT_EQ_char8t_pass)
+{
+    std::u8string a = u8"abc";
+    std::u8string b = u8"abc";
+    EXPECT_EQ(a, b);
+}
+
+TEST(psi_mock, detail_ptr_to_str)
+{
+    int x = 0;
+    const auto s = detail::ptr_to_str(&x);
+    EXPECT_TRUE(!s.empty());
+    const auto s2 = detail::ptr_to_str(nullptr);
+    EXPECT_TRUE(!s2.empty());
+}
+
+TEST(psi_mock, EXPECT_FALSE_pass)
+{
+    EXPECT_FALSE(false);
+}
+
+TEST(psi_mock, ASSERT_FALSE_pass)
+{
+    ASSERT_FALSE(false);
+}
+
+TEST(psi_mock, EXPECT_NE_shared_ptr_different)
+{
+    auto sp1 = std::make_shared<int>(1);
+    auto sp2 = std::make_shared<int>(2);
+    EXPECT_NE(sp1, sp2);
+}
+
+TEST(psi_mock, ASSERT_TRUE_pass)
+{
+    ASSERT_TRUE(true);
+}
+
+TEST(psi_mock, ASSERT_EQ_bool_pass)
+{
+    ASSERT_EQ(true, true);
+    bool x = false;
+    ASSERT_EQ(x, false);
+}
+
+TEST(psi_mock, ASSERT_EQ_string_pass)
+{
+    std::string a = "hello";
+    std::string b = "hello";
+    ASSERT_EQ(a, b);
+}
+
 } // namespace psi::test
